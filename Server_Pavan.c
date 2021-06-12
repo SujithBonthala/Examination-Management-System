b578da3d72a56348424d82af2a5816a1ddf1d696
SujithBonthala <78267490+SujithBonthala@users.noreply.github.com>
2021-06-12 21:26:09 +0530

    Update Server_Pavan.c

--- Server_Pavan.c
+++ Server_Pavan.c
@@ -1,1 +1,62 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "Header_File.h"
+void CreateTeacherAcc()
+{
+	FILE *fp;
+	fp=fopen("Teacher_Record.txt","a");
+	if(fp==NULL)
+	{
+		printf("Error...Try again\n");
+	}
+	else
+	{
+		teacher *t;
+		int n,i;
+		printf("\nEnter the number of teacher records that needs to get created : ");
+		scanf("%d",&n);
+		t=(teacher*)calloc(n,sizeof(teacher));
+		if(t==NULL)
+		{
+			printf("Error...Try again\n");
+		}
+		else
+		{
+			for(int i=0;i<n;i++)
+			{	
+				fflush(stdin);
+				printf("\nEnter teacher %d's name : ",i+1);
+				scanf("%[^\n]s",&t[i].Name);
+				fflush(stdin);
+				printf("Enter PRN of the teacher : ");
+				scanf("%[^\n]s",&t[i].PRN);
+				fflush(stdin);
+				strcpy(t[i].Password,t[i].PRN);
+				printf("Enter teacher's branch in short form using capital letters(Ex : CSE) : ");
+				scanf("%s",&t[i].Branch);
+				fflush(stdin);
+				fwrite(&t[i],sizeof(teacher),1,fp);
+			}
+		}
+	}
+	fclose(fp);
+}
+void NumberTeacherAcc()
+{
+	FILE *fp;
+	fp=fopen("Teacher_Record.txt","r");
+	fseek(fp,0,SEEK_END);
+	int n=ftell(fp)/sizeof(teacher);
+	printf("\nNumber of teacher records present is %d\n",n);
+	fclose(fp);
+}
+void SearchTeacherAcc()
+{
+}
+void UpdateTeacherAcc()
+{
+}
+void DeleteTeacherAcc()
+{
+}
