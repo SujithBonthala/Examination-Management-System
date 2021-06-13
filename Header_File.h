981f747c63a82d42d80a608fdd7bc538b49165b3
SujithBonthala <78267490+SujithBonthala@users.noreply.github.com>
2021-06-13 12:28:38 +0530

    Update Header_File.h

--- Header_File.h
+++ Header_File.h
@@ -1,7 +1,36 @@
-
 void LoginMenu();
 void GetAdminCredentials();
 void AdminMainMenu();
 void GetStudentCredentials();
 void GetTeacherCredentials();
 int Cipher(char un[],char pw[]);
+void CreateStudentAcc();
+void CreateTeacherAcc();
+void NumberStudentAcc();
+void NumberTeacherAcc();
+void SearchStudentAcc();
+void SearchTeacherAcc();
+void UpdateStudentAcc();
+void UpdateTeacherAcc();
+void DeleteStudentAcc();
+void DeleteTeacherAcc();
+void SetExamDetails();
+typedef struct student_records
+{
+	char Name[50];
+	char SRN[20];
+	char Password[50];
+	int Semester;
+	char Branch[10];
+	char Section;
+	int No_Days_Present;
+	int No_Days_Total;
+	int Eligibility;
+}student;
+typedef struct teacher_records
+{
+	char Name[50];
+	char PRN[20];
+	char Password[50];
+	char Branch[50];
+}teacher;
